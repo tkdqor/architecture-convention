f2be9f7dfd04cddf52fbad2b5e2b804303402e44
김상백 / Sangbaek Kim <aumsbk@naver.com>
2025-07-19 19:42:22 +0900

    feat: wrong case 수정

--- src/application/commerce/use-case/wrong-update-order.use-case.ts
+++ src/application/commerce/use-case/wrong-update-order.use-case.ts
@@ -14,9 +14,10 @@ export class WrongUpdateOrderUseCase {
     const entityManager = this.dataSource.manager;
     const order = await this.orderRepository.getById(entityManager, orderId);
     const orderItem = order.getItems()[0]; // Order 루트를 통하지 않고, 내부 OrderItem에 직접 접근
-    // orderItem.getQuantity = 10; // 직접 수량 변경 (불변식 위반)
-    // totalAmount를 업데이트하는 calculateTotalAmount()가 호출되지 않음
-    // Order의 totalAmount와 내부에 있는 OrderItem들의 실제 합계가 다르게 저장됨
+    // orderItem.quantity = 10; // 직접 수량 변경 (불변식 위반)
+    // orderItem.price = 10000; // 직접 가격 변경 (불변식 위반)
     // await orderItemRepository.save(entityManager, orderItem);
+    // totalAmount를 업데이트하는 Order의 calculateTotalAmount()가 호출되지 않음
+    // Order의 totalAmount와 내부에 있는 OrderItem들의 실제 합계가 다르게 저장됨
   }
 }
